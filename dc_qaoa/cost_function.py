c61c7d85dcd207d4526f9b1f5680364c2a7f3c99
TO-PRARAMX <nopparuj050206@gmail.com>
2026-03-03 17:25:01 +0700

    fix: change typename edge to Edge and replace 'type' with "TypeAlias" for better compatibiltiy with python 3.11

--- dc_qaoa/cost_function.py
+++ dc_qaoa/cost_function.py
@@ -1,10 +1,10 @@
 from . import config
 from pyquil import Program
-from .graph import edges
+from .graph import Edges
 
 import numpy as np
 
-def qaoa_cut_score(edges: edges, bitstring: list[0 | 1]) -> float:
+def qaoa_cut_score(edges: Edges, bitstring: list[0 | 1]) -> float:
     total_cut  = np.sum(
         w for (u, v, w) in edges if bitstring[u] != bitstring[v]
     )
